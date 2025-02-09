4735dae0120727a72ef86ad24edd742f47643083
makfluffy575 <fluffy.mk575@gmail.com>
2025-02-09 20:52:28 +0300

    new version

--- chat/index.js
+++ chat/index.js
@@ -11,6 +11,55 @@ const photoWrap = document.querySelector(".wrap-photo");
 
 const btn = document.querySelector(".btn");
 
+const yesAnswer= document.getElementById("answer-yes");
+
+const noAnswer= document.getElementById("answer-no");
+
+const nameInputWrap = document.querySelector(".wrap-input-name");
+
+yesAnswer.checked = true;
+
+
+//input message check
+const inputCheck = (evt) => {
+  if (evt.target.value == "") {
+    if (!btn.hasAttribute("disabled")) {
+      btn.toggleAttribute("disabled");
+    }
+  } else {
+      if (btn.hasAttribute("disabled")) {
+        btn.toggleAttribute("disabled");
+      }
+  }
+} 
+
+fieldForMessage.addEventListener('input', inputCheck);
+
+//checkbox YES
+const clickYes = () => {
+  if (yesAnswer.checked === true) {
+    noAnswer.checked = false;
+    nameInputWrap.classList.remove("wrap_none");
+  }
+  if (yesAnswer.checked === false) {
+    noAnswer.checked = true;
+    nameInputWrap.classList.add("wrap_none");
+  }
+}
+yesAnswer.addEventListener("click", clickYes);
+
+//checkbox NO
+const clickNo = () => {
+  if (noAnswer.checked === true) {
+    yesAnswer.checked = false;
+    nameInputWrap.classList.add("wrap_none");
+  }
+  if (noAnswer.checked === false) {
+    yesAnswer.checked = true;
+    nameInputWrap.classList.remove("wrap_none");
+  }
+}
+noAnswer.addEventListener("click", clickNo);
 
 
 //function
@@ -38,9 +87,20 @@ avatar.classList.add("photo");
 
 avatar.alt = "user photo";
 
-avatar.src = fieldForPhoto.value;
+if (fieldForPhoto.value == "") {
 
-fieldForPhoto.value = "";
+  const photos = ["./assets/img/photo1.jpg", "./assets/img/photo2.jpg", "./assets/img/photo3.jpg", "./assets/img/photo4.jpg", "./assets/img/photo5.jpg","./assets/img/photo6.jpg"];
+  
+  const randomIndex = Math.floor(Math.random() * photos.length);
+
+  avatar.src = photos[randomIndex];
+  
+} 
+else {
+  avatar.src = fieldForPhoto.value;
+
+  fieldForPhoto.value = "";
+}
 
 
 
@@ -51,15 +111,42 @@ nickName.classList.add("name");
 
 photoWrap.after(nickName);
 
-nickName.textContent = fieldForName.value.trim();
+if (fieldForName.value !== "") {
+  nickName.textContent = fieldForName.value.trim();
 
-fieldForName.value = "";
+  fieldForName.value = "";
 
-nickName.textContent = nickName.textContent[0].toUpperCase() + nickName.textContent.slice(1, nickName.length).toLowerCase();
+  nickName.textContent = nickName.textContent[0].toUpperCase() + nickName.textContent.slice(1, nickName.length).toLowerCase();}
+else {
+  nickName.textContent = "username";
+}
 
 
 
-//comment
+//chat
+//date
+const dateWrap = document.createElement("div");
+
+dateWrap.classList.add("wrap-date");
+
+messageWrap.before(dateWrap);
+
+const options = {
+  weekday: "short",
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+
+const dateMessage = new Date().toLocaleString("ru-RU", options); 
+
+dateWrap.innerText = dateMessage;
+
+//message
+
 const chatMessage = document.createElement("p");
 
 chatMessage.classList.add("message");
@@ -77,3 +164,4 @@ const checkSpam = (str) => {
 chatMessage.textContent = checkSpam(chatMessage.textContent);
 
 }
+btn.addEventListener("click", sendMessage);
